cdfacc76830ff27b24ddb537d8b92d81be4dedb3
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2022-10-08 12:16:56 -0300

    test(selection): adiciona testes de análise assintótica
    re #1

--- tests/test_selection_sort.py
+++ tests/test_selection_sort.py
@@ -1,6 +1,8 @@
 from sort_algorithms.selection import SelectionSort
+from random import sample
+from time import time
 
-def test_selection_sort(capsys):
+def test_steps_selection_sort(capsys):
     list = [9, 3, 6, 2, 9, 9, 5, 6, 10, 7]
     SelectionSort(list, print_steps=True).order()
     assert capsys.readouterr().out == (
@@ -15,3 +17,21 @@ def test_selection_sort(capsys):
         '[2, 3, 5, 6, 6, 7, 9, 9, 10, 9]\n'
         '[2, 3, 5, 6, 6, 7, 9, 9, 9, 10]\n'
     )
+
+def test_assintotic_unsorted():
+    list = [x for x in sample(range(10000), 10000)]
+    start_time = time()
+    SelectionSort(list).order()
+    print(f'Tempo de execução: {time() - start_time}')
+
+def test_assintotic_sorted():
+    list = [x for x in range(10000)]
+    start_time = time()
+    SelectionSort(list).order()
+    print(f'Tempo de execução: {time() - start_time}')
+
+def test_assintotic_reverse_sorted():
+    list = [x for x in range(10000, -1, -1)]
+    start_time = time()
+    SelectionSort(list).order()
+    print(f'Tempo de execução: {time() - start_time}')
