60ec8fdaedd00773b99ca468472e8d39c0bef443
Jose Noronha <jnoronha@uolinc.com>
2022-10-11 23:22:46 -0300

    test(selection): adiciona teste de lista com elementos repetidos
    re #1

--- tests/test_selection_sort.py
+++ tests/test_selection_sort.py
@@ -2,6 +2,7 @@ from sort_algorithms.selection import SelectionSort
 from random import sample
 from time import time
 
+
 def test_steps_selection_sort(capsys):
     list = [9, 3, 6, 2, 9, 9, 5, 6, 10, 7]
     SelectionSort(list, print_steps=True).order()
@@ -18,11 +19,13 @@ def test_steps_selection_sort(capsys):
         '[2, 3, 5, 6, 6, 7, 9, 9, 9, 10]\n'
     )
 
+
 def test_assintotic_unsorted():
     list = [x for x in sample(range(10000), 10000)]
     start_time = time()
     SelectionSort(list).order()
     print(f'Tempo de execução: {time() - start_time}')
+
 
 def test_assintotic_sorted():
     list = [x for x in range(10000)]
@@ -30,8 +33,17 @@ def test_assintotic_sorted():
     SelectionSort(list).order()
     print(f'Tempo de execução: {time() - start_time}')
 
+
 def test_assintotic_reverse_sorted():
     list = [x for x in range(10000, -1, -1)]
     start_time = time()
     SelectionSort(list).order()
     print(f'Tempo de execução: {time() - start_time}')
+
+
+def test_repeated_list():
+    list = [1 for _ in range(1000)]
+    start_time = time()
+    SelectionSort(list).order()
+    print(f'Tempo de execução: {time() - start_time}')
+
